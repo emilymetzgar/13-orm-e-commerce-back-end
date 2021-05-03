3a91f8e786ca9e01ba0d04f3ddd1a7c4ba2d07da
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-05-03 11:12:40 -0500

    working on find one in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,42 +1,41 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const {
+  Category,
+  Product
+} = require('../../models');
 
 // The `/api/categories` endpoint
 
 
-  // find all categories
-  // be sure to include its associated Products
-  router.get('/', async (req, res) => {
-    try {
-      const categoryData = await Category.findAll();
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+// find all categories
+// be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  router.get('/:id', async (req, res) => {
-    try {
-      const categoryData = await Category.findByPk(req.params.id, {
-        // JOIN with products, do I need this part?
-        //include: [{ model: Product, through: Trip, as: 'category_product' }]
-      });
-  
-      if (!locationData) {
-        res.status(404).json({ message: 'No category found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+// find one category by its `id` value
+// be sure to include its associated Products
+router.get('/:id', (req, res) => {
+  try {
+    Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Product]
+    }).then((response) => res.json(response))
+
+  } catch (err) {
+    res.status(400).json(err);
+
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
