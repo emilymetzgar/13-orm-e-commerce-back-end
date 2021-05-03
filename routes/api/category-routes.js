226a8fd175a21562360b258d3a2782f527e5a7d5
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-05-03 10:58:02 -0500

    working on get a single category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,26 @@ const { Category, Product } = require('../../models');
   });
 });
 
-router.get('/:id', (req, res) => {
+
   // find one category by its `id` value
   // be sure to include its associated Products
+  router.get('/:id', async (req, res) => {
+    try {
+      const categoryData = await Category.findByPk(req.params.id, {
+        // JOIN with products, do I need this part?
+        //include: [{ model: Product, through: Trip, as: 'category_product' }]
+      });
+  
+      if (!locationData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 });
 
 router.post('/', (req, res) => {
