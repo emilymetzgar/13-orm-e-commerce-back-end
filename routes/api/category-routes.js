5ef5d432e6b7997ed5113b9a0219ffcd02d861c1
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-05-03 11:16:45 -0500

    working on create category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,9 +37,18 @@ router.get('/:id', (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const createCategory = await Category.create({
+      category_name : req.body.category_name,
+    });
+    res.status(200).json(createCategory);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
